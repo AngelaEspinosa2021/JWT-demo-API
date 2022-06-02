1234efa6ccff07a5e4fe4d766812784502558f5f
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-01 23:37:11 -0500

    REFACTORIZACION: despues de millones de veces de borrar y volver a empezar, logramos la compilacion sin errores, pero falta la correcta generacion del token.

--- src/main/java/com/DEMOJWT/demo/service/UserService.java
+++ src/main/java/com/DEMOJWT/demo/service/UserService.java
@@ -1,15 +1,2 @@
-package com.DEMOJWT.demo.service;
-
-
-import com.DEMOJWT.demo.dto.User;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-public interface UserService {
-
-    public Flux<User> findAll();
-
-    public Mono<User> save(User user);
-
-    public Mono<User> findById (String id);
+package com.DEMOJWT.demo.service;public class UserService {
 }
